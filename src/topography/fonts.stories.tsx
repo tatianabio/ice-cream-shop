52de93881b18024a6f36ca65b1b2caa18e5d1c27
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-11-17 14:59:09 +0300

    Adding mixins for fonts

---
+++ src/topography/fonts.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './fonts.stories.scss';
+
+export default {
+  title: 'Topography/Fonts',
+};
+
+const headings = {};
+
+export const Demo = () => {
+  return (
+    <div>
+      <section className='headings'></section>
+    </div>
+  );
+};
