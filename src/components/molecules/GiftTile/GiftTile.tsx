8ecbd4411a12fed470ebdd4e9316aad24bdd1f5e
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-11-29 20:55:28 +0300

    Added ButtonLink to Gift Tile component instead of Button

--- src/components/molecules/GiftTile/GiftTile.tsx
+++ src/components/molecules/GiftTile/GiftTile.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './GiftTile.scss';
 import { useTranslation } from 'react-i18next';
-import Button from '../../atoms/Button';
 import { IGift } from '../../../mock/data/gifts';
+import ButtonLink from '../../atoms/ButtonLink';
 
 interface IGiftTile {
   /** Data of the gift: id, name, description, imgLink, link */
@@ -13,19 +13,18 @@ interface IGiftTile {
 
 const GiftTile = ({ data, 'data-testid': testId }: IGiftTile) => {
   const { name, description, imgLink, link } = data;
-  const onClickHandler = () => window.open(link, '_blank');
   const { t } = useTranslation();
   return (
     <div className='gift-tile'>
       <div className='gift-tile__info-wrapper'>
         <h3 className='gift-tile__title'>{t(name)}</h3>
         <p className='gift-tile__description'>{t(description)}</p>
-        <Button
-          className='gift-tile__more-details-button'
-          onClick={onClickHandler}
-          data-testid={`${testId}-button`}
+        <ButtonLink
           text={t('readGiftDetails')}
           variant='primary'
+          className='gift-tile__more-details-button'
+          data-testid={`${testId}-button`}
+          link={link}
         />
       </div>
       <img
