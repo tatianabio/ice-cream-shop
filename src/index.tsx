1318addbdb2b1bc82066740395335516eb0d6550
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-11-30 20:31:48 +0300

    Added routers in App

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './styles/index.scss';
 import './i18n';
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/home',
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: '/catalog',
+    element: <App />,
+  },
+  {
+    path: '*',
+    element: <Navigate replace to='/home' />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
