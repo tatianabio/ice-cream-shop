2bbcdcfbeb16e505520309e53a273acbcab28aa5
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-12-01 21:19:21 +0300

    Added the correct link for Figma design for Breadcrumbs component

--- src/components/atoms/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/components/atoms/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -10,7 +10,7 @@ export default {
   parameters: {
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/file/PGyn99Nrq2fXcnwGJqj0hR/HTML-1-%2F-%D0%93%D0%BB%D0%B5%D0%B9%D1%81%D0%B8-(34)-(Copy)?node-id=8153%3A3242&t=NLBtXuXkMTqBj2nE-4',
+      url: 'https://www.figma.com/file/PGyn99Nrq2fXcnwGJqj0hR/HTML-1-%2F-%D0%93%D0%BB%D0%B5%D0%B9%D1%81%D0%B8-(34)-(Copy)?node-id=8153%3A3258&t=NLBtXuXkMTqBj2nE-4',
     },
   },
 };
