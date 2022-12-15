f83d10f9bbee69da980de62858f7e49d12075261
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-12-15 20:30:34 +0300

    Added semantic tags in  Global Message component

--- src/components/atoms/GlobalMessage/GlobalMessage.tsx
+++ src/components/atoms/GlobalMessage/GlobalMessage.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import shallow from 'zustand/shallow';
+import { useTranslation } from 'react-i18next';
 import messageStore, { IMessageStore } from './GlobalMessage.store';
 
 const GlobalMessage = () => {
+  const { t } = useTranslation();
   const messagesToDisplay = messageStore((store: IMessageStore) => store.messagesToDisplay, shallow);
   const displayMessagesList = messagesToDisplay.map((item) => {
-    const { id, message } = item;
+    const { id, message, type } = item;
 
     return (
       <li key={id}>
-        <span>{message}</span>
+        <h3>{t(type)}</h3>
+        <span>{t(message)}</span>
       </li>
     );
   });
 
-  return <ul>{displayMessagesList}</ul>;
+  if (!messagesToDisplay.length) {
+    return <></>;
+  }
+
+  return (
+    <section>
+      <h2 className='visually-hidden'>{t('')}</h2>
+      <ul>{displayMessagesList}</ul>
+    </section>
+  );
 };
 
 export default GlobalMessage;
