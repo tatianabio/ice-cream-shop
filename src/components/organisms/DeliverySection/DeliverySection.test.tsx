319e4c2c087cfd82f52a29553361246d9ee7becc
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-12-26 22:12:49 +0300

    Added jest test for reset form in  DeliverySection component

--- src/components/organisms/DeliverySection/DeliverySection.test.tsx
+++ src/components/organisms/DeliverySection/DeliverySection.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import GlobalMessage from '../../atoms/GlobalMessage';
 import DeliverySection from './DeliverySection';
@@ -19,15 +19,18 @@ const TestComponent = () => {
 };
 
 describe('Delivery Section Tests', () => {
-  it('render and successful submission', async () => {
+  it('render and successful submission and reset of the form', async () => {
     render(<TestComponent />);
     expect(screen.getByTestId('demo-delivery-section')).toBeInTheDocument();
-    await userEvent.type(screen.getByTestId('demo-date-input'), '01.04.2020');
+    await userEvent.type(screen.getByTestId('demo-date-input'), '02.04.2020');
     await userEvent.type(screen.getByTestId('demo-phone-input'), '+1111111111');
     await userEvent.type(screen.getByTestId('demo-address-input'), 'test address');
     mswServer.use(postRequestWithoutDelaySuccess);
     await userEvent.click(screen.getByTestId('demo-button'));
     expect(await screen.findByTestId('demo-global-message')).toHaveClass('global-message__item--success');
+    await waitFor(() => {
+      expect(screen.getByTestId('demo-date-input')).toHaveValue('');
+    });
   });
 
   it('unsuccessful submission', async () => {
