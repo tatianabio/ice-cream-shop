a15a5357d401eda5cde5739a924ee504384d9586
tatianabio <ismagulova.t@gmail.com>
2023-02-01 14:20:30 -0500

    Added main tags and props for SectionTile component

--- src/components/organisms/SectionTile/SectionTile.tsx
+++ src/components/organisms/SectionTile/SectionTile.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
+
+interface ISectionTile {
+  /** The content of the popup */
+  title: string;
+  /** The content of the popup */
+  children: ReactElement;
+  /** Technical attributes */
+  'data-testid': string;
+}
 
 const SectionTile = () => {
-  return <div />;
+  const { t } = useTranslation();
+  return (
+    <section className='section'>
+      <h2 className='section__title'>{t('')}</h2>
+    </section>
+  );
 };
 
 export default SectionTile;
