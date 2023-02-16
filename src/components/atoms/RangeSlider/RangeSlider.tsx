30580acd1324289a303c9492db7b28ab72365957
tatianabio <ismagulova.t@gmail.com>
2023-02-16 18:44:56 -0500

    Added necessary attributes for Range Slider Component

--- src/components/atoms/RangeSlider/RangeSlider.tsx
+++ src/components/atoms/RangeSlider/RangeSlider.tsx
@@ -6,7 +6,16 @@ import './RangeSlider.scss';
 const RangeSlider = () => {
   return (
     <div className='range-slider'>
-      <Slider className='range-slider__wrapper' range />
+      <Slider
+        className='range-slider__wrapper'
+        range
+        min={3}
+        max={10}
+        step={0.5}
+        defaultValue={[4, 7.5]}
+        allowCross={false}
+        pushable={1}
+      />
     </div>
   );
 };
