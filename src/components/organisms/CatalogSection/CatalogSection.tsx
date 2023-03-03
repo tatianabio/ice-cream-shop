a723008a3c19dcda7e2c0fa1b4c360cbb1cd5c9a
tatianabio <ismagulova.t@gmail.com>
2023-03-03 15:59:29 -0500

    Applied initial filters in Catalog Section Component

--- src/components/organisms/CatalogSection/CatalogSection.tsx
+++ src/components/organisms/CatalogSection/CatalogSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './CatalogSection.scss';
 import { shallow } from 'zustand/shallow';
 import CatalogFilter from '../../molecules/CatalogFilter';
@@ -6,6 +6,10 @@ import CatalogList from '../../molecules/CatalogList';
 import useCatalogFilterStore, { ICatalogFilterStore } from '../../molecules/CatalogFilter/CatalogFilter.store';
 
 const CatalogSection = () => {
+  const setFilteredProductList = useCatalogFilterStore((store) => store.setFilteredProductList, shallow);
+
+  useEffect(() => setFilteredProductList(), []);
+
   const filteredProductList = useCatalogFilterStore((store: ICatalogFilterStore) => store.filteredProductList, shallow);
   return (
     <section className='catalog-section'>
