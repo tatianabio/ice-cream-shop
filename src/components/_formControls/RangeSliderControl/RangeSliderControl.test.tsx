37ccaad9f67818c27b1245c5b8ebc64a234a571b
tatianabio <ismagulova.t@gmail.com>
2023-04-12 15:49:21 -0400

    Updated jest tests for RangeSliderControl component

--- src/components/_formControls/RangeSliderControl/RangeSliderControl.test.tsx
+++ src/components/_formControls/RangeSliderControl/RangeSliderControl.test.tsx
@@ -6,10 +6,12 @@ describe('RangeSliderControl tests', () => {
   it('render', async () => {
     render(<Demo />);
     expect(screen.getByTestId('demo-form-field')).toBeInTheDocument();
+    expect(screen.getByTestId('demo-label')).toHaveTextContent('4');
     await userEvent.tab();
     await userEvent.keyboard('{ArrowRight}');
     await act(async () => {
       await userEvent.click(screen.getByTestId('demo-button'));
     });
+    expect(screen.getByTestId('demo-label')).toHaveTextContent('5');
   });
 });
